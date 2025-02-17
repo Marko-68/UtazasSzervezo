b38f6aa1dda7c7553dc102a62d716aaa0abee3a7
Márkó Gigler <giglermarko@gmail.com>
2025-02-17 10:44:06 +0100

    Add admin wpf

---
+++ UtazasSzervezo_Admin/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace UtazasSzervezo_Admin;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
