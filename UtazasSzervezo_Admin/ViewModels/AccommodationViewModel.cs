e1a28d4e39f3dfe37297d191203d4308ea8db1af
AkcMishxd <lengyel.misi06@gmail.com>
2025-04-02 19:56:32 +0200

    Slight changes

---
+++ UtazasSzervezo_Admin/ViewModels/AccommodationViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtazasSzervezo_Admin.Services;
+using UtazasSzervezo_Library.Models;
+
+namespace UtazasSzervezo_Admin.ViewModels
+{
+    public class AccommodationViewModel : BaseViewModel
+    {
+        private readonly ApiService _apiService;
+        private ObservableCollection<Accommodation> _accommodations;
+
+        public ObservableCollection<Accommodation> Accommodations
+        {
+            get => _accommodations;
+            set => SetProperty(ref _accommodations, value);
+        }
+
+        public AccommodationViewModel(ApiService apiService)
+        {
+            _apiService = apiService;
+            Accommodations = new ObservableCollection<Accommodation>();
+            LoadAccommodations();
+        }
+
+        private async void LoadAccommodations()
+        {
+            var accommodations = await _apiService.GetAsync<List<Accommodation>>("api/accommodation");
+            foreach (var accommodation in accommodations)
+            {
+                Accommodations.Add(accommodation);
+            }
+        }
+    }
+}
