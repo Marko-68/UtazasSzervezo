84e4ddb808a6caea27e964bc285eb6829b971fd8
Márkó Gigler <giglermarko@gmail.com>
2025-04-27 12:02:27 +0200

    Migrations

--- UtazasSzervezo_Library/Migrations/20250426151631_Removedescription.Designer.cs
+++ UtazasSzervezo_Library/Migrations/20250426151631_Removedescription.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UtazasSzervezo_Library;
 
@@ -11,9 +12,11 @@ using UtazasSzervezo_Library;
 namespace UtazasSzervezo_Library.Migrations
 {
     [DbContext(typeof(UtazasSzervezoDbContext))]
-    partial class UtazasSzervezoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250426151631_Removedescription")]
+    partial class Removedescription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UtazasSzervezo_Library/Migrations/20250426151631_Removedescription.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UtazasSzervezo_Library.Migrations
+{
+    /// <inheritdoc />
+    public partial class Removedescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "Bookings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "Bookings",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
